7fc69081dfc0770ee513aa027dc22f9f55650aa2
malavshah5 <malavnshah@hotmail.com>
2020-09-09 20:47:21 -0400

    Got grid aligned and staring to implement breadth first search

--- BFSCode.js
+++ BFSCode.js
@@ -1,60 +1,42 @@
 
-var size = 25;
-// did i do this correctly?
-var col = 25;
+const cellPref = "cell:";
 
 const gridInit = ()=> {
+    var size = 25;
     for(var r = 0; r < size; r++){
         var addRow = document.getElementById("grid");
         var row = document.createElement("div")
 
         row.id = "row" + (r);
-        row.style.display="flex"
-
+        row.className += "row";
+        
         for (var c = 0; c < size; c++) {
             var cell = document.createElement("div");
-            
-            // what is i
-            // cell.id = "cell" + i;
-            cell.style.backgroundColor = "white";
-            cell.style.display = "inline-block";
-            cell.style.margin = "0px";
-            cell.style.width = "15px";
-            cell.style.height = "15px";
-            cell.style.border = "1px black solid";
-            cell.style.paddingTop = "2px";
-            cell.style.touchAction = "none"
-            cell.style.touchAction = "pinch-zoom";
-
-            row.style.touchAction = "none";
-
+            cell.id = cellPref  + r.toString() + "_" + c.toString(); 
+            cell.className += "cell";
             row.appendChild(cell);
-            row.style.touchAction = "pinch-zoom";
-
-            //what is i?
-            // i++;
-        }
-        addRow.appendChild(row);
-
-
-        for (var c = 0; c < col; c++) {
-            // what is i?
-            // cell.id = "cell" + i;
-            cell.style.backgroundColor = "white";
-            cell.style.display = "inline-block";
-            cell.style.margin = "0px";
-            cell.style.width = "15px";
-            cell.style.height = "15px";
-            cell.style.border = "1px black solid";
-            cell.style.paddingTop = "2px";
-
-            row.style.touchAction = "pinch-zoom";
-            row.appendChild(cell);
-            // i++;
         }
         addRow.appendChild(row);
     }
-    
-}
+
+    setStart(0,0);
+    setDestination(size - 1, size - 1);
+
+};
+
+const setStart = (row, col) => {
+    const cellID = cellPref + row.toString() + "_" + col.toString();
+    const cellEle = document.getElementById(cellID); 
+    cellEle.className += " startingCell";
+};
+
+const setDestination = (row, col) => {
+    const cellID = cellPref + row.toString() + "_" + col.toString();
+    const cellEle = document.getElementById(cellID); 
+    cellEle.className += " targetCell";
+};
 
 gridInit();
+
+
+
